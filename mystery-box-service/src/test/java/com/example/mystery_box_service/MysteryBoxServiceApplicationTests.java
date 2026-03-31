adb89758c57ac1d3bcfaf49cda3b5a9339d45871
Timo Salm <accounts@timosalm.de>
2026-03-31 19:11:36 +0200

    Initial commit

---
+++ mystery-box-service/src/test/java/com/example/mystery_box_service/MysteryBoxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystery_box_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryBoxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
